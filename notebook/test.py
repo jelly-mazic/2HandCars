e3c96e2929760e62c1066e6804cf27b1a220ca66
evo_manjaro <tomazic@qq.com>
2020-04-21 22:06:14 +0800

    new start

--- notebook/test.py
+++ notebook/test.py
@@ -1,2 +1,8 @@
+import warnings
+warnings.filterwarnings('ignore')
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
 missing_list = ['bodyType','fuelType','gearbox']
